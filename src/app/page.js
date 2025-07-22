a0d78abbfb4b01e098541e8d79483a2a95469f08
M.BasilKhan <basilkhan1250@gmail.com>
2025-07-22 18:11:03 +0700

    workin on it

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div>
-        <h1>Hello world</h1>
+        <h1 className="text-3xl font-bold underline">Hello world</h1>
       </div>
     </>
   );
