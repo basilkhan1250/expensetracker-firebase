0f5ccb05f755585afcf4b28aee849c737b8da3fa
M.BasilKhan <basilkhan1250@gmail.com>
2025-07-23 18:20:48 +0700

    workin on it

--- src/app/page.js
+++ src/app/page.js
@@ -6,24 +6,26 @@ import { ImStatsBars } from "react-icons/im";
 export default function Home() {
   return (
     <>
-      <header className="flex items-center justify-between">
+      <header className="container max-w-2xl px-6 py-6 mx-auto" >
+        <div className="flex items-center justify-between">
 
-        {/* user information */}
+          {/* user information */}
 
-        <div className="flex items-center gap-2">
-          <div className="h-[50px] w-[50px] rounded-full overflow-hidden" >
-            <Image src={anime} alt="basil" className="object-cover w-full h-full" />
+          <div className="flex items-center gap-2">
+            <div className="h-[50px] w-[50px] rounded-full overflow-hidden" >
+              <Image src={anime} alt="basil" className="object-cover w-full h-full" />
+            </div>
+            <small>Hi, Basil</small>
           </div>
-          <small>Hi, Basil</small>
+
+          {/* right side of navigation */}
+
+          <nav className="flex items-center gap-2">
+            <div><ImStatsBars className="text-2xl" /></div>
+            <div><button className="btn">Sign out</button></div>
+          </nav>
+
         </div>
-
-        {/* right side of navigation */}
-
-        <nav className="flex items-center gap-2">
-          <div><ImStatsBars className="text-2xl" /></div>
-          <div><button className="btn">Sign out</button></div>
-        </nav>
-
       </header>
     </>
   );
