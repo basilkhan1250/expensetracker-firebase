6dd7c24834244101fe67143f222316d67008f3a4
M.BasilKhan <basilkhan1250@gmail.com>
2025-07-24 17:58:34 +0700

    workin on it

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,39 @@
 import { currencyFormatter } from "@/app/lib/utils"
+import ExpenseCatogoryItem from "@/app/components/ExpenseCatogoryItem"
+
+
+const DUMMY_DATE = [
+  {
+    id: 1,
+    title: "Entertainment",
+    color: "#613",
+    amount: 500
+  },
+  {
+    id: 2,
+    title: "Bike Repair",
+    color: "#483",
+    amount: 300
+  },
+  {
+    id: 3,
+    title: "Petrol",
+    color: "#519",
+    amount: 100
+  },
+  {
+    id: 4,
+    title: "House Paint",
+    color: "#353",
+    amount: 1000
+  },
+  {
+    id: 5,
+    title: "Laptop",
+    color: "#720",
+    amount: 700
+  },
+]
 
 export default function Home() {
   return (
@@ -19,14 +54,20 @@ export default function Home() {
 
         <section className="py-6">
           <h3 className="text-2xl">My Expenses</h3>
-          <div>
-            <div className="flex items-center justify-between">
-              <div className="flex items-center gap-2">
-                <div className="w-[25px] h-[25px] rounded-full bg-yellow-500" />
-                <h4 className="capitalize">Entertainment</h4>
-              </div>
-              <p>{currencyFormatter(500)}</p>
-            </div>
+
+          <div className="flex flex-col gap-4 mt-6">
+            {DUMMY_DATE.map((expense, id) => {
+              return (
+                <ExpenseCatogoryItem
+                  key={expense.id}
+                  color={expense.color}
+                  title={expense.title}
+                  amount={expense.amount}
+                />
+              )
+            })}
+
+
           </div>
         </section>
       </main>
